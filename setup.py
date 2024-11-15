c25df9b1f5e034c601be720fdeb41560cf8d296d
Shreyas Vinaya <vinaya.shreyas@gmail.com>
2024-11-15 23:55:08 +0530

    add python 3.11

--- setup.py
+++ setup.py
@@ -118,6 +118,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     keywords="project library deep-learning dft quantum-chemistry",
     zip_safe=False
